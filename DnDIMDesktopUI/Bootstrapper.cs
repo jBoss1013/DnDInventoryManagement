5f78ff7ced07b68c85b7b861c4d14622881ceb0c
Jeremy Boss <jrjlboss@outlook.com>
2021-01-28 17:04:02 -0600

    WPF LogIn screen
    added new window for login and set up loginViewModel to handle password box

--- DnDIMDesktopUI/Bootstrapper.cs
+++ DnDIMDesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using DnDIMDesktopUI.Helpers;
 using DnDIMDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace DnDIMDesktopUI
 {
@@ -16,6 +18,10 @@ namespace DnDIMDesktopUI
         public Bootstrapper()
         {
             Initialize();
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
         protected override void Configure()
         {
