09fdd60d957b86b53eada5620519c0328e8d170f
Jeremy Boss <jrjlboss@outlook.com>
2021-01-29 14:57:36 -0600

    Wired Up Login form
    Connected the login form button to API endpoint

--- DnDIMDesktopUI/Bootstrapper.cs
+++ DnDIMDesktopUI/Bootstrapper.cs
@@ -29,7 +29,9 @@ namespace DnDIMDesktopUI
 
             _container
                 .Singleton<IWindowManager, WindowManager>()
-                .Singleton<IEventAggregator, EventAggregator>();
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<IAPIHelper, APIHelper>();
+
 
             GetType().Assembly.GetTypes().Where(type => type.IsClass).Where(type => type.Name.EndsWith("ViewModel"))
                 .ToList().ForEach(viewModelType => _container.RegisterPerRequest(
---
+++ DnDIMDesktopUI/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using DnDIMDesktopUI.Models;
+using System.Threading.Tasks;
+
+namespace DnDIMDesktopUI.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string userName, string password);
+    }
+}
